059fe09ac619d3d9104c4496206c249cf86b8ba7
jangjiheee <jihee.jang210@gmail.com>
2022-11-02 18:05:01 +0900

    main 메뉴 수정

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 window.onload = function() {
-  var proBtn = document.querySelector(".project_flex");
-  var changeI = document.querySelector(".flex_img");
+  var changeI = document.querySelector(".proBtn");
   var hiddenL = document.querySelector(".project-list");
-  proBtn.addEventListener("click", function () {
-    changeI.classList.toggle("click");
+  var ghostBtn = document.querySelector(".ghost")
+  var ghostList = document.querySelector(".ghost-list");
+
+  
+  changeI.addEventListener("click", function () {
+    this.classList.toggle("click");
+    if (changeI.classList.contains('click')) {
+      hiddenL.style.display = "block";
+      hiddenL.style.height = "100%";
+      hiddenL.style.marginTop = "20px";
+    } else {
+      hiddenL.style.height = "0px";
+      hiddenL.style.marginTop = "0";
+    }
   })
-
-  if (!document.querySelector('.flex_img').classList.contains('click')) return;
-  // {
-  //   hiddenL.style.display = "none";
-  // }
 }
-
-// function proBtn() {
-//   var cngI = document.querySelector(".flex_img");
-//   cngI.toggle.classList.add("click");
-//   console.log(cngI);
-// }
