29548b0cbbc1163185f138209902472a72ac8fcf
hwayungyi90 <hwayoung0716@gmail.com>
2022-11-07 18:21:34 +0900

    스크롤 애니메이션 스크립트 수정

--- js/section2.js
+++ js/section2.js
@@ -7,34 +7,32 @@ var swiper = new Swiper(".mySwiper", {
 
 // 텍스트 밑줄 애니메이션
 
-let titText = document.querySelectorAll(".tit_underline");
-let proText = document.querySelectorAll(".project_underline");
-let newsText = document.querySelectorAll('.news_underline');
-let inqText = document.querySelectorAll('.inquire_underline');
-
-
 window.addEventListener("scroll", function () {
+  // let titText = document.querySelectorAll(".tit_underline");
+  let proText = document.querySelector(".project_underline");
+  let newsText = document.querySelector('.news_underline');
+  let inqText = document.querySelector('.inquire_underline');
   let value = window.scrollY;
-  console.log('123');
+  // console.log('123');
 
   // 첫 번째 섹션 스크롤이벤트
   if (value > 100) {
-    proText[0].classList.add('active');
+    proText.classList.add('active');
   } else {
-    proText[0].classList.remove('active');
+    proText.classList.remove('active');
   };
 
   //두 번째 섹션 스크롤이벤트
   if (value > 1100) {
-    newsText[0].classList.add('active');
+    newsText.classList.add('active');
   } else {
-    newsText[0].classList.remove('active');
+    newsText.classList.remove('active');
   };
 
   //세 번째 섹션 스크롤이벤트
   if (value > 2100) {
-    inqText[0].classList.add('active');
+    inqText.classList.add('active');
   } else {
-    inqText[0].classList.remove('active');
+    inqText.classList.remove('active');
   }
 });
