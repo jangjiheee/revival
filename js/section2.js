d1344a2cd6c9d4bf319b57e498ce42c8754dd01e
hyeeun <jhe1209@gmail.com>
2022-11-08 10:45:34 +0900

    recycle 섹션 작업

--- js/section2.js
+++ js/section2.js
@@ -9,6 +9,7 @@ var swiper = new Swiper(".mySwiper", {
 
 window.addEventListener("scroll", function () {
   // let titText = document.querySelectorAll(".tit_underline");
+  let cycleText = document.querySelector(".recycle_underline");
   let proText = document.querySelector(".project_underline");
   let newsText = document.querySelector('.news_underline');
   let inqText = document.querySelector('.inquire_underline');
@@ -17,20 +18,27 @@ window.addEventListener("scroll", function () {
 
   // 첫 번째 섹션 스크롤이벤트
   if (value > 100) {
+    cycleText.classList.add('active');
+  } else {
+    cycleText.classList.remove('active');
+  };
+
+  //두 번째 섹션 스크롤이벤트
+  if (value > 1000) {
     proText.classList.add('active');
   } else {
     proText.classList.remove('active');
   };
 
-  //두 번째 섹션 스크롤이벤트
-  if (value > 1100) {
+  //세 번째 섹션 스크롤이벤트
+  if (value > 1800) {
     newsText.classList.add('active');
   } else {
     newsText.classList.remove('active');
   };
 
-  //세 번째 섹션 스크롤이벤트
-  if (value > 2100) {
+  //네 번째 섹션 스크롤이벤트
+  if (value > 2700) {
     inqText.classList.add('active');
   } else {
     inqText.classList.remove('active');
