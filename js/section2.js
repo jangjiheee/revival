193753b7e3d4a7e0223fd694c0ec15abdb21d55e
hyeeun <jhe1209@gmail.com>
2022-11-09 15:58:24 +0900

    메인, 공통 css, js 수정 (밑줄, 스크롤이벤트 수정)

--- js/section2.js
+++ js/section2.js
@@ -16,31 +16,65 @@ window.addEventListener("scroll", function () {
   let value = window.scrollY;
   // console.log('123');
 
-  // 첫 번째 섹션 스크롤이벤트
-  if (value > 100) {
-    cycleText.classList.add('active');
-  } else {
-    cycleText.classList.remove('active');
-  };
+  if (window.innerWidth >= 768) {
+    // 첫 번째 섹션 스크롤이벤트
+    if (value > 100) {
+      cycleText.classList.add('active');
+    } else {
+      cycleText.classList.remove('active');
+    };
 
-  //두 번째 섹션 스크롤이벤트
-  if (value > 1000) {
-    proText.classList.add('active');
-  } else {
-    proText.classList.remove('active');
-  };
+    //두 번째 섹션 스크롤이벤트
+    if (value > 1000) {
+      proText.classList.add('active');
+    } else {
+      proText.classList.remove('active');
+    };
 
-  //세 번째 섹션 스크롤이벤트
-  if (value > 1800) {
-    newsText.classList.add('active');
-  } else {
-    newsText.classList.remove('active');
-  };
+    //세 번째 섹션 스크롤이벤트
+    if (value > 1800) {
+      newsText.classList.add('active');
+    } else {
+      newsText.classList.remove('active');
+    };
 
-  //네 번째 섹션 스크롤이벤트
-  if (value > 2700) {
-    inqText.classList.add('active');
-  } else {
-    inqText.classList.remove('active');
+    //네 번째 섹션 스크롤이벤트
+    if (value > 2700) {
+      inqText.classList.add('active');
+    } else {
+      inqText.classList.remove('active');
+    }
+  }
+
+  //모바일사이즈
+
+  if (window.innerWidth < 768) {
+    // 첫 번째 섹션 스크롤이벤트
+    if (value > 100) {
+      cycleText.classList.add('active');
+    } else {
+      cycleText.classList.remove('active');
+    };
+
+    //두 번째 섹션 스크롤이벤트
+    if (value > 1000) {
+      proText.classList.add('active');
+    } else {
+      proText.classList.remove('active');
+    };
+
+    //세 번째 섹션 스크롤이벤트
+    if (value > 1400) {
+      newsText.classList.add('active');
+    } else {
+      newsText.classList.remove('active');
+    };
+
+    //네 번째 섹션 스크롤이벤트
+    if (value > 2700) {
+      inqText.classList.add('active');
+    } else {
+      inqText.classList.remove('active');
+    }
   }
 });
